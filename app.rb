33035ee4a2df52320e562f6661adff8d1013b23c
Julio Cesar Ody <julioody@gmail.com>
2011-02-23 19:18:37 +1100

    stuff

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'sinatra'
+
 Bundler.require :default, Sinatra::Application.environment
 
-Mongoid.configure do |config|
-  environment = Sinatra::Application.environment.to_s
-  config.from_hash YAML.load_file(File.dirname(__FILE__) + '/mongoid.yml')[environment]
-end
+# Mongoid.configure do |config|
+#   environment = Sinatra::Application.environment.to_s
+#   config.from_hash YAML.load_file(File.dirname(__FILE__) + '/mongoid.yml')[environment]
+# end
