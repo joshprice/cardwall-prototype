a281da231d6a67cafcead16f9f749d3191e52860
Julio Cesar Ody <julioody@gmail.com>
2011-02-23 19:40:03 +1100

    body instead of entries

--- public/js/cards.js
+++ public/js/cards.js
@@ -10,12 +10,8 @@
     function makeBody() {
       that.body = $('' +
       '<form class="card" id="card_1">' +
-        '<h1>' + this.title + '</h1>' +
-        '<ul>' +
-          '<li>Good stuff</li>' +
-          '<li>Bad stuff</li>' +
-          '<li>Normal stuff</li>' +
-        '</ul>' +
+        '<h1>' + that.title + '</h1>' +
+        '<p>' + that.body + '</p>' +
       '</form>');
     }
     $('#wall').append(that.body);
