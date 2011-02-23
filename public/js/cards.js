c7f2f1cda63c82b30a805c9b5f662e13cffe55bd
Julio Cesar Ody <julioody@gmail.com>
2011-02-23 20:45:41 +1100

    backboned

--- public/js/cards.js
+++ public/js/cards.js
@@ -1,19 +1,76 @@
-(function() {
-  Card = function(properties) {
-    var that = this;
-    this.title      = properties.title || '';
-    this.body       = properties.body || '';
-    this.dimensions = properties.dimensions || '';
+(function() {  
+  Card = Backbone.Model.extend({
+    defaults: {
+      title :   'title...',
+      body  :   'body...'
+    },
     
-    makeBody();
+    initialize: function() {
+      if (!this.get('title')) this.set({ 'title': this.defaults.title });
+      if (!this.get('body')) this.set({ 'title': this.defaults.body });      
+    }    
+  });
+  
+  CardCollection = Backbone.Collection.extend({
+    model: Card,
+    localStorage: new Store('cards')
+  });
+  
+  Cards = new CardCollection;
+  
+  CardView = Backbone.View.extend({
+    tagName   : 'div',
+    template  : _.template($('#card-template').html()),
     
-    function makeBody() {
-      that.body = $('' +
-      '<form class="card" id="card_1">' +
-        '<h1>' + that.title + '</h1>' +
-        '<p>' + that.body + '</p>' +
-      '</form>').draggable();
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.view = this;
+    },
+    
+    render : function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      $(this.el).addClass('card').appendTo('body');
     }
-    $('#wall').append(that.body);
-  };
+  });
+  
+  AppView = Backbone.View.extend({
+    el  : $('#wall'),
+    
+    initialize: function() {
+      _.bindAll(this, 'addOne', 'addAll');
+      Cards.bind('refresh', this.addAll);
+      Cards.bind('add', this.addOne);
+      Cards.fetch();
+    },
+        
+    addOne: function(card) {
+      var view = new CardView({model: card});
+      view.render();
+    },
+    
+    addAll: function() {
+      Cards.each(this.addOne);
+    }
+  });
+  
+  Application = new AppView;
+  
+  
+  // Card = function(properties) {
+  //   var that = this;
+  //   this.title      = properties.title || '';
+  //   this.body       = properties.body || '';
+  //   this.dimensions = properties.dimensions || '';
+  //   
+  //   makeBody();
+  //   
+  //   function makeBody() {
+  //     that.body = $('' +
+  //     '<form class="card" id="card_1">' +
+  //       '<h1>' + that.title + '</h1>' +
+  //       '<p>' + that.body + '</p>' +
+  //     '</form>').draggable();
+  //   }
+  //   $('#wall').append(that.body);
+  // };
 })();
